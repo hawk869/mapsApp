9d3a510bcd9be9b01e7906a17232ae1817bca85f
Daniel Esteban <decepeda@misena.edu.co>
2024-02-08 18:57:32 -0500

    Manteniendo el arreglo de marcadores y colores

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+interface MarkerAndColor {
+  color: string,
+  marker: Marker
+}
+
+
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
@@ -9,6 +15,7 @@ export class MarkersPageComponent {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public markers: MarkerAndColor[] = [];
   public map?: Map;
   public currentLngLat: LngLat = new LngLat(-74.09644109195581, 4.702687959471163);
 
@@ -53,5 +60,12 @@ export class MarkersPageComponent {
     })
       .setLngLat( lngLat )
       .addTo( this.map )
+
+      this.markers.push({ color, marker });
+  }
+
+  deleteMarker( index: number ) {
+    this.markers[index].marker.remove();
+    this.markers.splice( index, 1 );
   }
 }
