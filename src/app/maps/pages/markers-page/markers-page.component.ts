aaece4f93612f56d3e557d69e314d7dbe041f78c
Daniel Esteban <decepeda@misena.edu.co>
2024-02-12 19:48:21 -0500

    Se actualiza el storage cuando se mueve el marcador

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -70,6 +70,10 @@ export class MarkersPageComponent {
 
       this.markers.push({ color, marker });
       this.saveToLocalStorage();
+
+      marker.on('dragend', () => {
+        this.saveToLocalStorage();
+      })
   }
 
   deleteMarker( index: number ) {
